b028b08ef774b52c89e50bbc7d16b88ae7ef4907
Sandy Tao <sandytao520@icloud.com>
2025-08-07 09:18:53 -0700

    Fix(core): Use Flash for next speaker check (#5786)

--- packages/core/src/utils/nextSpeakerChecker.ts
+++ packages/core/src/utils/nextSpeakerChecker.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Content } from '@google/genai';
+import { Content, SchemaUnion, Type } from '@google/genai';
 import { DEFAULT_GEMINI_FLASH_MODEL } from '../config/models.js';
 import { GeminiClient } from '../core/client.js';
 import { GeminiChat } from '../core/geminiChat.js';
