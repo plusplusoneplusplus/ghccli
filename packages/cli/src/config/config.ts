5aac3e53e709e08a4a0bbc568bde06620423f913
shrutip90 <shruti.p90@gmail.com>
2025-08-07 14:06:17 -0700

    Add new folderTrust setting that the users can enable or disable (#5798)

--- packages/cli/src/config/config.ts
+++ packages/cli/src/config/config.ts
@@ -363,6 +363,8 @@ export async function loadCliConfig(
     argv.ideModeFeature ?? settings.ideModeFeature ?? false;
 
   const folderTrustFeature = settings.folderTrustFeature ?? false;
+  const folderTrustSetting = settings.folderTrust ?? false;
+  const folderTrust = folderTrustFeature && folderTrustSetting;
 
   const allExtensions = annotateActiveExtensions(
     extensions,
@@ -548,6 +550,7 @@ export async function loadCliConfig(
     // === END GHCCLI ===
     chatCompression: settings.chatCompression,
     folderTrustFeature,
+    folderTrust,
   });
 }
 
